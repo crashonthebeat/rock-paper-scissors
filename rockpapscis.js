dcf4706dd7e463d2cafb22a848ddcc3437ce05ef
crashonthebeat <64339619+crashonthebeat@users.noreply.github.com>
2023-01-20 23:31:32 -0800

    Added reset functionality, cleaned up CSS.
    Added functionality to the reset button, and had the page display
    default values upon loading.
    
    I also gave the reset button a max width and the same CSS classes
    as the other apps, so that when I include this page in the rest of
    my landing page it will be consistent.

--- rockpapscis.js
+++ rockpapscis.js
@@ -1,9 +1,29 @@
-const prompt = require('prompt-sync')();
-
 const options = ['rock', 'paper', 'scissors']
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const resetGameButton = document.querySelector('#reset');
+
+const gameResult = document.querySelector('#game-result')
+const gameResultContent = document.createElement('div');
+gameResultContent.classList.add('content');
+
+const finalResult = document.querySelector('#final-result');
+const finalResultContent = document.createElement('div');
+gameResultContent.classList.add('content');
+
+const playerScore = document.querySelector('#player-score');
+const playerScoreContent = document.createElement('div');
+playerScoreContent.classList.add('content');
+
+const cpuScore = document.querySelector('#cpu-score');
+const cpuScoreContent = document.createElement('div');
+playerScoreContent.classList.add('content');
+
 let score = 0;
-let cpuScore = 0;
+let computerScore = 0;
+let defaultMessage = 'Please make a selection to play.'
 
 function getCPUChoice () {
     let intchoice = Math.floor(Math.random() * 2);
@@ -14,29 +34,51 @@ function playRound(player, cpu) {
     if (player == cpu) { result = 'It\'s a tie!'; } 
     else if (player == 'rock') {
         if (cpu == 'scissors') { result = 'You win! Rock beats scissors.'; score++;}
-        else {result = 'You lose! Paper covers rock.'; cpuScore++;}
+        else {result = 'You lose! Paper covers rock.'; computerScore++;}
     } else if (player == 'paper') {
         if (cpu == 'rock') {result = 'You win Paper covers rock!'; score++;} 
-        else {result = 'You lose! Scissors cuts paper.'; cpuScore++;}
+        else {result = 'You lose! Scissors cuts paper.'; computerScore++;}
     } else {
         if (cpu == 'paper') {result = 'You win! Scissors cuts paper!'; score++;} 
-        else { result = 'You lose! Rock beats scissors.'; cpuScore++;}
+        else { result = 'You lose! Rock beats scissors.'; computerScore++;}
     }
     return result
 }
 
-function game() {
-    const playerSelection = prompt("Please choose rock paper or scissors. ");
+function game(choice) {
     const cpuSelection = getCPUChoice();
 
-    console.log(playRound(playerSelection, cpuSelection));
+    gameResultContent.textContent = playRound(choice, cpuSelection);
+    gameResult.appendChild(gameResultContent);
+
+    playerScoreContent.textContent = score;
+    playerScore.appendChild(playerScoreContent);
+
+    cpuScoreContent.textContent = computerScore;
+    cpuScore.appendChild(cpuScoreContent);
 }
 
-while (score < 7 && cpuScore < 7) {
-    game()
+function resetGame() {
+    let score = 0;
+    let computerScore = 0;
+
+    playerScoreContent.textContent = score;
+    playerScore.appendChild(playerScoreContent);
+
+    cpuScoreContent.textContent = computerScore;
+    cpuScore.appendChild(cpuScoreContent);
+
+    gameResultContent.textContent = defaultMessage;
+    gameResult.appendChild(gameResultContent);
 }
 
-console.log("Your final score is: " + score + ". The computer's score is " + cpuScore + ".");
+resetGame();
+
+rock.onclick = () => game('rock');
+paper.onclick = () => game('paper');
+scissors.onclick = () => game('scissors');
+resetGameButton.onclick = () => resetGame();
+
 
 
 
